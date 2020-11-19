6c3b74b437dd83265f914f7313271b803231111c
Arnau <arnaugesapascual1999@gmail.com>
2020-11-19 01:34:19 +0100

    ABEMUS MENÚ (per fi xD)

---
+++ BarGo/app/src/main/java/com/example/bargo/ListBarFragment.java
@@ -0,0 +1,17 @@
+package com.example.bargo;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class ListBarFragment extends Fragment {
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_list_bar, container, false);
+        return  view;
+    }
+}
