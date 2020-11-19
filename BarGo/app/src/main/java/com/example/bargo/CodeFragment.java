ba5f56725a6bd6cb4c59f432292a15a4e41a972d
metabit1000 <aslexag0@gmail.com>
2020-11-19 19:06:14 +0100

    Error solved and messages introduced

--- BarGo/app/src/main/java/com/example/bargo/CodeFragment.java
+++ BarGo/app/src/main/java/com/example/bargo/CodeFragment.java
@@ -1,18 +1,45 @@
 package com.example.bargo;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
 public class CodeFragment extends Fragment {
+    EditText introducedCode;
+    Button button;
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
+        final View rootView = inflater.inflate(R.layout.fragment_code, container, false);
+
+        introducedCode = (EditText)rootView.findViewById(R.id.code);
+        button = (Button)rootView.findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /* Pasar a una altre pantalla
+                  Intent i = new Intent (MainActivity.this, MapsActivity.class);
+                  startActivity(i);
+                */
+                
+                String code = introducedCode.getText().toString();
+                if (TextUtils.isEmpty(code)) Toast.makeText(getActivity().getBaseContext(), "Introduzca un código valido", Toast.LENGTH_SHORT).show();
+                else Toast.makeText(getActivity().getBaseContext(), "¡Canjeado!", Toast.LENGTH_SHORT).show();
+
+            }
+        });
 
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_code, container, false);
+        return rootView;
     }
 }
