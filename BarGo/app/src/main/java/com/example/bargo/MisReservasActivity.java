0ffc2766843504a4041db2b4ca58f8cca1acd94f
annallanza <annallanza@hotmail.com>
2020-12-02 18:29:11 +0100

    Mis Reservas

---
+++ BarGo/app/src/main/java/com/example/bargo/MisReservasActivity.java
@@ -0,0 +1,88 @@
+package com.example.bargo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MisReservasActivity extends AppCompatActivity {
+
+    private ListView reservasListView;
+    private ArrayList<Integer> barsListImages;
+    private ArrayList<String> barsListNames;
+    private ArrayList<String> barsListAdresses;
+    private ArrayList<String> barsListReservationInfo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mis_reservas);
+
+        barsListImages = MisReservasInfo.getInstance().getImages();
+        barsListNames = MisReservasInfo.getInstance().getBars();
+        barsListAdresses = MisReservasInfo.getInstance().getAdresses();
+        barsListReservationInfo = MisReservasInfo.getInstance().getInfoReserva();
+
+        reservasListView = findViewById(R.id.mis_reservasList);
+        AdaptadorMisReservasList adaptador =  new AdaptadorMisReservasList();
+        reservasListView.setAdapter(adaptador);
+
+    }
+
+    class AdaptadorMisReservasList extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return barsListImages.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(final int position, View convertView, ViewGroup parent) {
+            View view = getLayoutInflater().inflate(R.layout.reserva_item_list, null);
+
+            ImageView barImage = view.findViewById(R.id.barImage);
+            TextView barName = view.findViewById(R.id.barNameText);
+            TextView barAdress = view.findViewById(R.id.adressText);
+            TextView barReservationInfo = view.findViewById(R.id.reservaText);
+            Button cancelButton = view.findViewById(R.id.cancelButton);
+
+            barImage.setImageResource(barsListImages.get(position));
+            barName.setText(barsListNames.get(position));
+            barAdress.setText(barsListAdresses.get(position));
+            barReservationInfo.setText(barsListReservationInfo.get(position));
+
+            cancelButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    MisReservasInfo.getInstance().cancelReserva(position);
+                    Toast.makeText(MisReservasActivity.this, "Se ha cancelado la reserva", Toast.LENGTH_SHORT).show();
+                    Intent mis_reservas_activity = new Intent(MisReservasActivity.this, MisReservasActivity.class);
+                    startActivity(mis_reservas_activity);
+                    finish();
+                }
+            });
+
+            return view;
+        }
+    }
+}
--- BarGo/app/src/main/java/com/example/bargo/profileFragment.java
+++ BarGo/app/src/main/java/com/example/bargo/profileFragment.java
@@ -1,18 +1,23 @@
 package com.example.bargo;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
 public class profileFragment extends Fragment {
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        Button mis_reservas = (Button) view.findViewById(R.id.misReservas_button);
 
         TextView userName = (TextView) view.findViewById(R.id.user_name_profile);
         userName.setText(User.getInstance().getName());
@@ -20,6 +25,14 @@ public class profileFragment extends Fragment {
         TextView points = (TextView) view.findViewById(R.id.points);
         String p = String.valueOf(User.getInstance().getPoints());
         points.setText(p + " " + getString(R.string.points));
+
+        mis_reservas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mis_reservas_activity = new Intent(getActivity(), MisReservasActivity.class);
+                startActivity(mis_reservas_activity);
+            }
+        });
 
         return view;
     }
